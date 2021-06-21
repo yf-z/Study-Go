33122e497419baa0aca5ac09bca2443bbc35c9e0
yifan zhang <yfzh@ucdavis.edu>
2021-06-21 15:07:20 +0800

    update

--- golang/chapter_2_program_structure/chap2Excer/chap2Excer.go
+++ golang/chapter_2_program_structure/chap2Excer/chap2Excer.go
@@ -1,5 +1,7 @@
 package chap2Excer
 
+import "fmt"
+
 type Celsius float64
 type Fahrenheit float64
 type Kelvin float64
@@ -10,3 +12,7 @@ const (
 	BoilingC Celsius = 100
 	ZeroK Kelvin = -273.15
 )
+
+func (c Celsius) String() string {return fmt.Sprintf("%g°C", c)}
+func (f Fahrenheit) String() string {return fmt.Sprintf("%g°F", f)}
+func (k Kelvin) String() string {return fmt.Sprintf("%g°K", k)}
