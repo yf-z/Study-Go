0a75a014e6e73299272c04ce1f7dafb360ebd3d1
yifan zhang <yfzh@ucdavis.edu>
2021-06-21 15:27:08 +0800

    Update prob_2_1.go

--- golang/chapter_2_program_structure/chap2Excer/prob_2_1.go
+++ golang/chapter_2_program_structure/chap2Excer/prob_2_1.go
@@ -12,7 +12,7 @@ func cToK(c Celsius) Kelvin {return Kelvin(c+Celsius(ZeroK))}
 func kToF(k Kelvin) Fahrenheit {return cToF(kToC(k))}
 func fToK(f Fahrenheit) Kelvin {return cToK(fToC(f))}
 
-func solve_2_1() {
+func Solve_2_1() {
 	fmt.Println("Convert K to C: ", kToC(Kelvin(1)))
 	fmt.Println("Convert C to K: ", cToK(Celsius(1)))
 	fmt.Println("Convert K to F: ", kToF(Kelvin(1)))
