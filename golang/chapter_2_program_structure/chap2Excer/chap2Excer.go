abd4a9f2a30ce61e11185d93732616a76bb7a1fa
yifan zhang <yfzh@ucdavis.edu>
2021-06-21 10:42:11 +0800

    update

--- golang/chapter_2_program_structure/chap2Excer/chap2Excer.go
+++ golang/chapter_2_program_structure/chap2Excer/chap2Excer.go
@@ -12,16 +12,3 @@ const {
 	BoilingC Celsius = 100
 	ZeroK Kelvin = -273.15
 }
-
-func (c Celsius) String() string {return fmt.Sprintf("%v°C", c)}
-func (f Fahrenheit) String() string {return fmt.Sprintf("%v°F", f)}
-func (k Kelvin) String() string {return fmt.Sprintf("%v°K", k)}
-
-func cToF(c Celsius) Fahrenheit {return Fahrenheit(c*9/5+32)}
-func fToC(f Fahrenheit) Celsius {return Celsius((f-32)*5/9)}
-
-func kToC(k Kelvin) Celsius {return Celsius(k-ZeroK)}
-func cToK(c Celsius) Kelvin {return Kelvin(c+ZeroK)}
-
-func kToF(k Kelvin) Fahrenheit {return cToF(kToC)}
-func fToK(f Fahrenheit) Kelvin {return cToK(fToC)}
